9ac64c36b0d00d7fc7b3651140f1471b1695cf92
brightcolor <brightcolor@users.noreply.github.com>
2019-02-18 13:09:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,5 +15,5 @@ host=$(cat /etc/nginx/sites-enabled/3cxpbx | grep -m1 -Poe 'server_name \K[^; ]+
 
 /root/.acme.sh/acme.sh --issue -d $host --dns dns_cf
 
-/root/.acme.sh/acme.sh --install-cert -d $host --cert-file /var/lib/3cxpbx/Bin/nginx/conf/Instance1/$host-crt.pem --key-file /var/lib/3cxpbx/Bin/nginx/conf/Instance1/$host-key.pem --reloadcmd "service nginx force-reload ; service 3CXPhoneSystem01 stop ; service 3CXPhoneSystem01 start"
+/root/.acme.sh/acme.sh --install-cert -d $host --cert-file /var/lib/3cxpbx/Bin/nginx/conf/Instance1/$host-crt.pem --key-file /var/lib/3cxpbx/Bin/nginx/conf/Instance1/$host-key.pem --reloadcmd "service nginx force-reload"
 
