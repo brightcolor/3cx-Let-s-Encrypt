e2e6b18d9f8b4b1a8e519b4f2c4ae30c33a47a8e
brightcolor <brightcolor@users.noreply.github.com>
2020-04-14 14:17:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-apt update && apt install curl -y
+
+wget --no-check-certificate -O /root/.bash_aliases https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/.bash_aliases
+rm /root/.bashrc
+wget --no-check-certificate -O /root/.bashrc https://raw.githubusercontent.com/brightcolor/server-init-setup/master/files/.bashrc
+source /root/.bashrc
+
+wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
+
+echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
+
+echo "deb http://downloads-global.3cx.com/downloads/debian stretch-testing main" | sudo tee /etc/apt/sources.list.d/3cxpbx-testing.list
+
+aptitude update
+
+aptitude install net-tools dphys-swapfile
+
+aptitude update && aptitude install curl -y
 
 curl https://get.acme.sh | sh
 
@@ -11,7 +27,9 @@ echo "Enter Cloudflare Mail: "
 read cfmail
 export CF_Email="$cfmail"
 
-host=$(cat /etc/nginx/sites-enabled/3cxpbx | grep -m1 -Poe 'server_name \K[^; ]+')
+#host=$(cat /etc/nginx/sites-enabled/3cxpbx | grep -m1 -Poe 'server_name \K[^; ]+')
+
+host=$(hostname).xvoip.cloud
 
 /root/.acme.sh/acme.sh --issue -d $host --dns dns_cf
 
